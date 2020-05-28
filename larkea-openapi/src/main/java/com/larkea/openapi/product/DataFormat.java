bfabdbd2b4aca8e61bb33b9fd6c7242dc9795d37
Le Wang <thisiswangle@gmail.com>
2020-05-28 09:50:10 +0800

    add wentek data format

--- larkea-openapi/src/main/java/com/larkea/openapi/product/DataFormat.java
+++ larkea-openapi/src/main/java/com/larkea/openapi/product/DataFormat.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 
 @Getter
 public enum DataFormat implements EnumData {
-    HUI_LINK_JSON(1, "慧通标准数据格式"),
-    USER(2, "自定义/透传");
+	HUI_LINK_JSON(1, "慧通标准数据格式"),
+	WEINTEK_JSON(2, "威纶通数据格式"),
+	USER(99999, "自定义/透传");
 
     Integer value;
 
