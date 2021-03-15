13e4fe87bf77bc20aaa4074ad0caf5c1c8c7bca0
Le Wang <lewang.dev@gmail.com>
2021-03-15 17:52:01 +0800

    添加获取产品列表

--- larkea-openapi/src/main/java/com/larkea/openapi/LarkeaClient.java
+++ larkea-openapi/src/main/java/com/larkea/openapi/LarkeaClient.java
@@ -3,10 +3,11 @@ package com.larkea.openapi;
 import java.util.List;
 
 import com.larkea.boot.core.data.Page;
-import com.larkea.boot.core.data.PageQueryParam;
 import com.larkea.openapi.device.DeviceInfo;
 import com.larkea.openapi.device.DevicePageQueryParam;
 import com.larkea.openapi.device.DeviceStatusInfo;
+import com.larkea.openapi.product.ProductInfo;
+import com.larkea.openapi.product.ProductPageQueryParam;
 import com.larkea.openapi.thing.CommandInfo;
 import com.larkea.openapi.thing.EventInfo;
 import com.larkea.openapi.thing.OperationInfo;
@@ -24,52 +25,55 @@ import feign.RequestLine;
 
 public interface LarkeaClient {
 
-    @RequestLine("GET /devices")
-    Page<DeviceInfo> listDevices(@QueryMap DevicePageQueryParam devicePageQueryParam);
+	@RequestLine("GET /products")
+	Page<ProductInfo> listProducts(@QueryMap ProductPageQueryParam productPageQueryParam);
 
-    @RequestLine("GET /devices/{deviceId}")
-    DeviceInfo getDeviceById(@Param("deviceId") Long deviceId);
+	@RequestLine("GET /devices")
+	Page<DeviceInfo> listDevices(@QueryMap DevicePageQueryParam devicePageQueryParam);
 
-    @RequestLine("GET /devices/device_info?productKey={productKey}&deviceKey={deviceKey}")
-    DeviceInfo getDeviceByPkAndDk(@Param("productKey") String productKey, @Param("deviceKey") String deviceKey);
+	@RequestLine("GET /devices/{deviceId}")
+	DeviceInfo getDeviceById(@Param("deviceId") Long deviceId);
 
-    @RequestLine("GET /devices/{deviceId}/status")
-    DeviceStatusInfo getDeviceStatusById(@Param("deviceId") Long deviceId);
+	@RequestLine("GET /devices/device_info?productKey={productKey}&deviceKey={deviceKey}")
+	DeviceInfo getDeviceByPkAndDk(@Param("productKey") String productKey, @Param("deviceKey") String deviceKey);
 
-    @RequestLine("GET /things/model?productKey={pk}")
-    ThingModel getThingModelByProductKey(@Param("pk") String pk);
+	@RequestLine("GET /devices/{deviceId}/status")
+	DeviceStatusInfo getDeviceStatusById(@Param("deviceId") Long deviceId);
 
-    @RequestLine("GET /things/properties/{propertyId}")
-    PropertyInfo getProperty(@Param("propertyId") Long propertyId);
+	@RequestLine("GET /things/model?productKey={pk}")
+	ThingModel getThingModelByProductKey(@Param("pk") String pk);
 
-    @RequestLine("GET /things/commands/{commandId}")
-    CommandInfo getCommand(@Param("commandId") Long commandId);
+	@RequestLine("GET /things/properties/{propertyId}")
+	PropertyInfo getProperty(@Param("propertyId") Long propertyId);
 
-    @RequestLine("GET /things/events/{eventId}")
-    EventInfo getEvent(@Param("eventId") Long eventId);
+	@RequestLine("GET /things/commands/{commandId}")
+	CommandInfo getCommand(@Param("commandId") Long commandId);
 
-    @RequestLine("GET /timeseries/properties/{propertyId}?deviceId={deviceId}")
-    Page<TsPropertyKvEntry> listTsPropertyData(
+	@RequestLine("GET /things/events/{eventId}")
+	EventInfo getEvent(@Param("eventId") Long eventId);
+
+	@RequestLine("GET /timeseries/properties/{propertyId}?deviceId={deviceId}")
+	Page<TsPropertyKvEntry> listTsPropertyData(
 			@Param("deviceId") Long deviceId,
 			@Param("propertyId") Long propertyId,
-            @QueryMap TsPropertyDataPageQueryParam param);
+			@QueryMap TsPropertyDataPageQueryParam param);
 
-    @RequestLine("GET /timeseries/properties")
-    Page<TsPropertyKvEntry> listTsPropertyData(@QueryMap BatchTsPropertyDataPageQueryParam param);
+	@RequestLine("GET /timeseries/properties")
+	Page<TsPropertyKvEntry> listTsPropertyData(@QueryMap BatchTsPropertyDataPageQueryParam param);
 
-    @RequestLine("GET /timeseries/devices/{deviceId}/keys")
-    Page<TsPropertyKvEntry> listDeviceTsPropertyData(
-            @Param("deviceId") Long deviceId,
-            @QueryMap DeviceTsPropertyDataPageQueryParam param);
+	@RequestLine("GET /timeseries/devices/{deviceId}/keys")
+	Page<TsPropertyKvEntry> listDeviceTsPropertyData(
+			@Param("deviceId") Long deviceId,
+			@QueryMap DeviceTsPropertyDataPageQueryParam param);
 
-    @RequestLine("GET /things/properties/values?productKey={pk}&deviceKey={dk}")
-    List<TsData> getDevicePropertyValues(@Param("pk") String pk, @Param("dk") String dk);
+	@RequestLine("GET /things/properties/values?productKey={pk}&deviceKey={dk}")
+	List<TsData> getDevicePropertyValues(@Param("pk") String pk, @Param("dk") String dk);
 
-    @RequestLine("GET /things/properties/values/batch?deviceIdList={deviceIdList}")
-    List<TsPropertyKvEntry> listDevicePropertyValues(@Param("deviceIdList") String deviceIdList);
+	@RequestLine("GET /things/properties/values/batch?deviceIdList={deviceIdList}")
+	List<TsPropertyKvEntry> listDevicePropertyValues(@Param("deviceIdList") String deviceIdList);
 
-    @RequestLine("GET /products/{productId}/operations")
-    List<OperationInfo> listProductOperations(@Param("productId") Long productId);
+	@RequestLine("GET /products/{productId}/operations")
+	List<OperationInfo> listProductOperations(@Param("productId") Long productId);
 
 	@RequestLine("POST /messages/publish")
 	@Headers("Content-Type: application/x-www-form-urlencoded")
